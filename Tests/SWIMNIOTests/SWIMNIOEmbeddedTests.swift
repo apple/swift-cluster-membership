1bb921dae3672bfc9d633677b718d7c9154a3b22
Konrad `ktoso` Malawski <konrad_malawski@apple.com>
2020-08-24 22:49:01 +0900

    +swimnio embedded test that drops all pings

--- Tests/SWIMNIOTests/SWIMNIOEmbeddedTests.swift
+++ Tests/SWIMNIOTests/SWIMNIOEmbeddedTests.swift
@@ -41,6 +41,38 @@ final class SWIMNIOEmbeddedTests: EmbeddedClusteredXCTestCase {
         XCTAssertEqual(second.swim.allMemberCount, 2)
     }
 
+    func test_embedded_suspicionsBecomeDeadNodesAfterTime() throws {
+        let first = self.makeEmbeddedShell("first") { settings in
+            settings.swim.initialContactPoints = [secondNode]
+            settings.swim.node = firstNode
+        }
+        let second = self.makeEmbeddedShell("second") { settings in
+            settings.swim.initialContactPoints = []
+            settings.swim.node = secondNode
+        }
+
+        for _ in 0 ... 5 {
+            self.loop.advanceTime(by: .seconds(1))
+            self.exchangeMessages(first, second)
+        }
+
+        XCTAssertEqual(first.swim.allMemberCount, 2)
+        XCTAssertEqual(second.swim.allMemberCount, 2)
+
+        // --- simulate cluster partition ---
+        var foundSuspects = false
+        var rounds = 1
+        while !foundSuspects {
+            self.loop.advanceTime(by: .seconds(1))
+            self.timeoutPings(first, second)
+            // the nodes can't send each other messages, and thus eventually emit dead warnings
+            foundSuspects = first.swim.suspects.count == 1 && second.swim.suspects.count == 1
+            rounds += 1
+        }
+
+        print("  Becoming suspicious of each other after a cluster partition took: \(rounds) rounds")
+    }
+
     private func exchangeMessages(_ first: SWIMNIOShell, _ second: SWIMNIOShell) {
         let firstEmbeddedChannel = first.channel as! EmbeddedChannel
         let secondEmbeddedChannel = second.channel as! EmbeddedChannel
@@ -50,6 +82,34 @@ final class SWIMNIOEmbeddedTests: EmbeddedClusteredXCTestCase {
         }
         if let writeCommand = try! secondEmbeddedChannel.readOutbound(as: WriteCommand.self) {
             first.receiveMessage(message: writeCommand.message)
+        }
+    }
+
+    /// Timeout pings between nodes
+    private func timeoutPings(_ first: SWIMNIOShell, _ second: SWIMNIOShell) {
+        let firstEmbeddedChannel = first.channel as! EmbeddedChannel
+        let secondEmbeddedChannel = second.channel as! EmbeddedChannel
+
+        guard let writeCommand1 = try! firstEmbeddedChannel.readOutbound(as: WriteCommand.self) else {
+            return
+        }
+        switch writeCommand1.message {
+        case .ping(_, _, let sequenceNumber):
+            first.receivePingResponse(response: .timeout(target: second.peer, pingRequestOrigin: nil, timeout: .milliseconds(1), sequenceNumber: sequenceNumber), pingRequestOriginPeer: nil)
+        default:
+            // deliver others as usual
+            second.receiveMessage(message: writeCommand1.message)
+        }
+
+        guard let writeCommand2 = try! secondEmbeddedChannel.readOutbound(as: WriteCommand.self) else {
+            return
+        }
+        switch writeCommand2.message {
+        case .ping(_, _, let sequenceNumber):
+            second.receivePingResponse(response: .timeout(target: second.peer, pingRequestOrigin: nil, timeout: .milliseconds(1), sequenceNumber: sequenceNumber), pingRequestOriginPeer: nil)
+        default:
+            // deliver others as usual
+            first.receiveMessage(message: writeCommand2.message)
         }
     }
 }
